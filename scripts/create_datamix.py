7a9bdc72d5ae71cd31d7630cec1b2d1d698f1dd4
thiomajid <tchiombianomajid@gmail.com>
2025-07-13 12:55:21 +0100

    Fixed data parsing

--- scripts/create_datamix.py
+++ scripts/create_datamix.py
@@ -2,6 +2,7 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 
 from src.data.mixture import DataMixConfig, MixDatasetConfig, push_datamix_to_hub
+from src.data.rules import BinaryFilterRule
 
 
 @hydra.main(config_path="../configs", config_name="data_mix_config", version_base="1.2")
@@ -13,7 +14,18 @@ def main(cfg: DictConfig):
         print("Config dict created successfully")
 
         config = DataMixConfig(**config_dict)
-        config.data_mix = [MixDatasetConfig(**el) for el in config.data_mix]
+
+        # convert inner dicts to dataclasses
+        config.data_mix = [MixDatasetConfig(**mix_dict) for mix_dict in config.data_mix]
+
+        # convert filter_rules
+        for mix_config in config.data_mix:
+            if mix_config.filter_rules:
+                mix_config.filter_rules = [
+                    BinaryFilterRule(**rule_dict)
+                    for rule_dict in mix_config.filter_rules
+                ]
+
         print("config loaded")
 
         push_datamix_to_hub(config)
