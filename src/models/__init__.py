53dd1daa0c835deb52403ce037deea86206b919a
thiomajid <tchiombianomajid@gmail.com>
2025-07-26 23:35:05 +0100

    Finalized HF model script

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,3 +3,16 @@ JAX/Flax model implementations.
 
 This module contains JAX implementations of transformer models using Flax NNX.
 """
+
+import typing as tp
+
+from flax import nnx
+
+from .gemma2.modeling_gemma2 import Gemma2ForCausalLM
+
+MODEL_REGISTRY: dict[str, tp.Type[nnx.Module]] = {"gemma2": Gemma2ForCausalLM}
+
+__all__ = [
+    "MODEL_REGISTRY",
+    "Gemma2ForCausalLM",
+]
